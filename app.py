c5fa5b77291d63559d0f745f87def246851da8db
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2021-03-21 17:40:43 +0000

    Add on the login page functionality

--- app.py
+++ app.py
@@ -50,6 +50,27 @@ def signup():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # check if the username is occupied in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure the hash password it was matced with the user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # wrong password matched
+                flash("Wrong Username and/or Password")
+                return redirect(url_for("login"))
+        
+        else:
+            # username dont exist in the db
+            flash("Wrong Username and/or Password")
+        return redirect(url_for("login"))                     
+
     return render_template("login.html")
 
 
