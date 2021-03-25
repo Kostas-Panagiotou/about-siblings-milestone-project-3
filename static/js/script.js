add724bb53fd4adac20775d865b32a04ac1f83dc
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2021-03-25 12:40:58 +0000

    initial commit

--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
     $('.sidenav').sidenav({edge: "right"});
     $('.parallax').parallax();
     $('.collapsible').collapsible();
+    $('input#input_text, textarea#textarea2').characterCounter();
 });
 
