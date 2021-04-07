aa8f6fb7a4a21f0e3439b80c9fe15271f19bde5e
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2021-04-07 08:15:44 +0000

    Initial commit

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ if os.path.exists("env.py"):
     import env
 import bson
 
-
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
@@ -249,4 +248,4 @@ def internal_server(error):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
